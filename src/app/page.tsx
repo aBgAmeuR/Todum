34c36c005d788c9dd9583f7724132ded3593cc82
aBgAmeuR <a.josset@yahoo.com>
2023-10-23 18:53:22 +0200

    Add utility function and input component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
     <>
       <h1>Todum</h1>
       <Button text="Ajouter" />
+      <Input type="text" placeholder="Ajouter une nouvelle tâche"/>
     </>
   )
 }
