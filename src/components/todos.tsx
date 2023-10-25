2cb1620b4842b5d49b30b50b2e39e606186755fb
aBgAmeuR <a.josset@yahoo.com>
2023-10-25 13:00:37 +0200

    Add type definition for Todo in Todos component

--- src/components/todos.tsx
+++ src/components/todos.tsx
@@ -9,9 +9,16 @@ export const Todos = async () => {
     },
   })
 
+  type Todo = {
+    id: string;
+    createdAt: Date;
+    content: string;
+    done: boolean;
+  }
+
   return (
     <div className='flex flex-col gap-3 w-full mb-8'>
-      {todo.map((todo) => (
+      {todo.map((todo: Todo) => (
         <Todo key={todo.id} todo={todo} />
       ))}
     </div>
